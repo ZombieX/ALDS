178f5eb705efe4ecfd5823d8b3cc23df7ce88dce
zxc <ZombieX@users.noreply.github.com>
2019-01-17 11:37:18 +0900

    Rewrite
    The source did NOT work. So it is replaced new one that work on AOJ.

--- 1_3_C_DoubleLinkedList/Source.cpp
+++ 1_3_C_DoubleLinkedList/Source.cpp
@@ -1,240 +1,313 @@
 
 #include<iostream>
-#include<utility>
-
-
-template<class>
-class node;
-
-template<class >
-class dlist;//double linked list
-
-
+#include<string>
+#include<vector>
+#include<memory>
 
 template<class T>
-class node {
-	T value_;
-	node<T>*p, *n;//prev, next
-public:
+struct node {
+	T key;
+	node* parent;
+	std::unique_ptr<node<T>> left, right;
+	bool valid_;
+
+
+	bool nil()const {
+		return !valid_;
+	}
+
+	bool valid()const {
+		return valid_;
+	}
+
+	void validate() {
+		valid_ = true;
+	}
+
+	void invalidate() {
+		this->valid_ = false;
+	}
+
+	node() :key{}
+		, parent{ nullptr }
+		, left{ nullptr }
+		, right{ nullptr }
+		, valid_{ false }
+	{}
+
 	template<class X>
-	node(X&& v_, node<T>*const prev_, node<T>* const next_)
-		:value_{ std::forward<X>(v_) }, p{ prev_ }, n{ next_ } {}
-	auto is_head()const { this->prev == nullptr; }
-	auto is_tail()const { this->next == nullptr; }
-	auto value()const& { return this->value_; }
-	//auto operator()()& { return this->value; }
-	auto value() && { return std::move(this->value_); }
-	auto& next(node<T>*const np) { n = np; return *this;}
-	auto& prev(node<T>*const pp) { p = pp; return *this; }
-	auto* next()const { return this->n; }
-	auto* prev()const { return this->p; }
+	node(X&&t) :key{ t }
+		, parent{ nullptr }
+		, left{ nullptr }
+		, right{ nullptr }
+		, valid_{ false }
+	{}
 
 };
 
+
+
 template<class T>
-class dlist {
-	node<T>* np,*last;
-	std::size_t len;
+void inorder(const node<T>* const root) {
+	if (root == nullptr)
+		return;
 
-public:
-	dlist() :np{ nullptr }, last{nullptr}, len{ 0 } {}
+	inorder(root->left.get());
+	std::cout << " " << root->key;
+	inorder(root->right.get());
+}
+
+template<class T>
+void preorder(const node<T>* const root) {
+	if (root == nullptr)
+		return;
+
+	std::cout << " " << root->key;
+	preorder(root->left.get());
+	preorder(root->right.get());
+}
 
 
-	template<class X>
-	auto& insert(X&&x) {
-		if (this->size() == 0) {
-			np = new node<T>{
-				std::forward<X>(x)
-				,nullptr
-				,nullptr
-			};
-			last = np;
+template<class T>
+void print(const node<T>*const root) {
+	inorder(root);
+	std::cout << "\n";
+	preorder(root);
+	std::cout << "\n";
+}
+
+
+template<class T>
+void insert(node<T>* root, const T&value_) {
+	decltype(root) parent = nullptr;
+	decltype(root) here = root;
+
+	auto z = std::make_unique<node<T>>(value_);
+	z->validate();
+
+	//値が最も近い、有効なノード(parent)へ移動
+	while (here != nullptr && here->valid()) {
+		parent = here;
+		//		std::cout << here->key << " ";
+
+		if (z->key < here->key) {
+			here = here->left.get();
+			//			std::cout << "> ";
 		}
 		else {
-
-			auto* new_ = new node<T>{
-				std::forward<X>(x)
-				,np
-				,np->next()
-			};
-			np->next(new_);
-//			new_->prev()
-//			np->(new_);
-//			new_->next(np);
-//			np = new_;
-			last = new_
-				;
+			here = here->right.get();
+			//			std::cout << "< ";
 		}
-		len++;
-		return *this;
+		//		std::cout << value_ << "\n";
+	}
+
+	//zはparentの left か right になる
+	z->parent = parent;
+
+	auto& up
+		= z->key < parent->key ?
+		parent->left
+		: parent->right;
+
+	up = std::move(z);
+
+}
+
+template<class T>
+auto find(node<T>* root, const T& value_)->node<T>* {
+	if (root == nullptr)
+		return nullptr;
+
+	if (root->key == value_)
+		return root;
+
+	auto& child = value_ < root->key ? root->left : root->right;
+	return find(child.get(), value_);
+
+}
+
+
+template<class T>
+auto& which(node<T>*np) {
+	return  np->left  ? np->left : np->right;
+}
+
+template<class T>
+auto& which(node<T>*np,const T&key) {
+	return np->left->key == key
+		? np->left : np->right;
+}
+
+
+template<class T>
+auto del(node<T>*node) {
+	auto lb = !!node->left;
+	auto rb = !!node->right;
+
+	auto chs = (lb ? 1 : 0) + (rb ? 1 : 0);
+
+	auto& p = node->parent;
+	if(node->parent != nullptr)
+	if (chs == 0) {
 		/*
-		np = new node<T>{
-			std::forward<X>(x)
-			,len == 0 ? nullptr : np + len - 1
-			,nullptr
-		};
+		auto&dying = p->left->key == node->key
+			? p->left : p->right;
+		dying.reset();
+		*/
+		//親のLとRでKeyが同じほうを消去
+//		std::cout << "chs == 0\n";
+		if (p->left && p->right)
+			which(p, node->key).reset();
+		else
+			(p->left && !p->right ? p->left : p->right).reset();
+	}
+	else if (chs == 1) {
 
-		if (len != 0) {
-			auto* prev = (np + len - 1);
-			auto tmp = node<T>{
-				std::forward<X>(x)
-				,prev->prev()
-				,np
-			};
-			*prev = tmp;
-		}
-		len++;
+		//std::cout << "chs == 1\n";
+
+		//どちらかだけ有効、もう一方はnullptr
+		//childの親を書き換え
+		auto&child = which(node);
+		auto& cp = child->parent;
+		//parentのchildを書き換え
+		cp = node->parent;
+
+		/*
+		auto& p = child->parent;
+		p->left->key == node->key
+			? p->left : p->right
+			= std::move(child);*/
+		
+		/*
+		std::cout << "node " << node->key << "\n";
+		std::cout <<"node->parent "<< child->parent->key << "\n";
+		std::cout <<"node->child "<< child->key << "\n";
 		*/
 
-		return *this;
+		//少なくとも一方はnode自身であるので有効なはず。
+		//両方とも有効ならnode->keyが同じほう(つまりnode)を、
+		//一方のみ有効なら有効な方(つまりnode)にchildをmove
+		/*
+		if(cp->left == nullptr){
+			std::cout << "cp->left == null\n";
+		}
+		if (cp->right == nullptr) {
+			std::cout << "cp->right == null\n";
+		}
+		*/
+		/*
+		if (cp->left && cp->right) {
+			std::cout <<"LR "<< cp->left->key << " " << cp->right->key << "\n";
+			std::cout << "this is me >> "<<which(cp, node->key)->key << "\n";
+		}*/
+
+		auto& dying_node = cp->left && cp->right ?
+			which(cp, node->key)
+			: cp->left != nullptr ?
+			cp->left : cp->right;
+
+		//std::cout <<"dying "<< dying_node->key << "\n";
+		dying_node = std::move(child);
+		/*
+		if (cp->left && cp->right) {
+			std::cout << "LR " << cp->left->key << " " << cp->right->key << "\n";
+			//std::cout << "this is me >> " << which(cp, node->key)->key << "\n";
+		}*/
+		
+	}
+	else {
+	//	node->key<p->key
+//		std::cout << "chs == 2\n";
+
+		//first <- child, second <- successor
+		auto& nl = node->left;
+		auto& nr = node->right;
+		auto b = node->key < node->parent->key;
+
+
+//		std::cout << "nl->key " << nl->key << "\n";
+//		std::cout << "nr->key " << nr->key << "\n";
+
+//		std::cout <<"comp node vs node->parent "<<
+//			node->key << " " << node->parent->key << "\n";
+
+		auto& child = b ? nr : nl;
+
+//		std::cout << "child " << child->key << "\n";
+
+		
+//		std::cout << "suc is " << (b ? "nl" : "nr") << " side\n";
+		auto& suc = b ?
+			(nl->right != nullptr ? nl->right : nl)
+			: (nr->left != nullptr ? nr->left : nr);
+
+//		std::cout << "nr->left->key " << nr->left->key << "\n";
+//		std::cout << "succ " << suc->key<<"\n";
+
+
+		//link switch ver
+		//消えるノードの親と子を後継者に渡す。
+		//親を渡す
+		/*
+		suc->parent = node->parent;
+
+		//消えるノードの2つの子のうち、後継者にならないほうは、後継者の子となる
+		(b ? suc->left : suc->right) = std::move(child);
+
+		//子を渡す
+		which(node->parent,node->key) = std::move(suc);
+		*/
+
+		// こっちでもいい。けどkeyの=が高コストな時は良くないだろう。
+		// assignment ver
+
+		node->key = suc->key;
+		suc.reset();
+		
 	}
 
-	template<class X>
-	bool del(X&&x) {
-		if (0 == len) {
-			return false;
-		}
-		else {
-			auto* here = this->np;
-			if (here->value() == x) {
-				if (this->size() == 1)
-					delete here;
-				else {
-					auto n = here->next();
-					this->np = n;
-					np->prev(nullptr);
-					delete here;
-				}
-				--len;
-				return true;
-			}
-
-			for (std::size_t i{1}; i < this->size() ; ++i) {
-				if (here == nullptr) return false;
-				if (here->value() == x)break;
-				here = here->next();
-			}
-			auto* n = here->next();
-			auto* p = here->prev();
-			delete here;
-			
-			if(p != nullptr) p->next(n);
-			if(n != nullptr) n->prev(p);
-			else this->last = n;
-			--len;
-			return true;
-			/*
-			if (np->value() == x) {
-				auto* p = here->prev();
-				auto* n = here->next();
-				p->next(n);
-
-				if(n!=nullptr)n->prev(p);
-				delete np;
-				len--;
-				return true;
-			}
-			else return  false;*/
-		}
-	}
-	auto size()const { return len; }
-	auto head()const { return np; }
-
-	auto delfirst() {
-		if (this->size() == 0)return false;
-		if (this->size() == 1) {
-			delete this->np;
-		}
-		else {
-			auto*n = this->np->next();
-			n->prev(nullptr);
-			delete np;
-			np = n;
-		}
-		len--;
-		return true;
-	}
-
-	auto dellast() {
-		if (this->size() == 0)return false;
-
-		auto*p = this->last;
-		if (p->prev() == nullptr)
-			delete p;
-		else {
-			auto *pre = p->prev();
-			pre->next(nullptr);
-			last = pre;
-			delete p;
-		}
-		return true;
-	}
-
-
-	auto empty()const { this->size() == 0; }
-
-	auto& print()const {
-		auto here = np;
-		if (here == nullptr) return*this;
-
-		for (std::size_t i{}; i < this->size(); ++i) {
-			std::cout << here->value() << " ";
-			here = here->next();
-		}
-		std::cout << "\n";
-		return *this;
-	}
-
-
-	~dlist() {
-
-		auto* here = np;
-		auto* next = (here)->next();
-		while (here!=nullptr &&next != nullptr) {
-			delete here;
-			here = next;
-			next = here->next();
-		}
-
-	}
-
-};
-
-
-
-auto test() {
-
-	std::cout << "Del \n";
-	dlist<int> d;
-
-	for (int i{}; i < 5; i++)d.insert(i);
-	std::cout << d.size() << " ";
-	d.print();
-
-	for (int i{}; i < 5; i += 2)d.del(i), std::cout << "s,i " << d.size() << " " << i << "\n";
-	d.print();
-
-}
-
-auto test2() {
-	std::cout << "Del first\n";
-	dlist<int> d;
-	for (int i{ 5 }; i < 50; i += 10)d.insert(i);
-	for (std::size_t i{}; i < d.size(); i++)d.delfirst(),d.print();
-}
-
-auto test3() {
-
-	std::cout << "Del last\n";
-	dlist<int> d;
-	for (int i{ 2 }; i < 50; i += 10)d.insert(i);
-	for (std::size_t i{}; i < d.size(); i++)d.dellast(),d.print();
 }
 
 int main() {
-	test();
-	test2();
-	test3();
+	int n;
+	std::cin >> n;
+
+	//	std::vector<node<int>> tree;
+	//	tree.emplace_back(node<int>{});
+	std::unique_ptr<node<int>> root{ nullptr };
+
+
+	for (int i{}; i < n; ++i) {
+		std::string s;
+		std::cin >> s;
+		if (s == "insert") {
+			int k;
+			std::cin >> k;
+			if (!root) {
+				root = std::make_unique<node<int>>(k);
+				root->validate();
+			}
+			else
+				insert(root.get(), k);
+		}
+		else if (s == "find") {
+			int k;
+			std::cin >> k;
+			std::cout << (find(root.get(), k) ? "yes" : "no") << "\n";
+		}
+		else if (s == "delete") {
+			int k;
+			std::cin >> k;
+			auto*p = find(root.get(), k);
+			/*if (p == nullptr) {
+				std::cout << ">>>>  error\n";
+			}
+			else*/
+				del(p);
+		}
+		else
+			print(root.get());
+	}
+
 	return 0;
 }
-
